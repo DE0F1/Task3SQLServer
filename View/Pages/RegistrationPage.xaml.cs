da86cb4ebcf37e41a589daab9ca7487511cf5490
deofi <deofi@DESKTOP-E5PHOB4>
2023-04-25 07:40:13 +0300

    Добавьте файлы проекта.

---
+++ View/Pages/RegistrationPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task3SQLServer.Core;
+
+namespace Task3SQLServer.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            CoreConnection.CoreFrame.Navigate(new MainLoginPage());
+        }
+    }
+}
